e922d96bed4bd8663253cd416e37528c20a22dfb
MBAMER <163824331+MBAMER@users.noreply.github.com>
2024-09-17 17:22:10 +0200

    modification code
    modification code drone

---
+++ exos/Drones/Drones/Drones/Drones/Model/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System.Drawing;
+
+namespace building
+{
+    public class Building
+    {
+        // Attributs du bâtiment
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Depth { get; set; }
+        public Color Color { get; set; }
+
+        // Constructeur
+        public Building(int x, int y, int width, int depth, Color color)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Depth = depth;
+            Color = color;
+        }
+
+        // Méthode pour dessiner le bâtiment
+        public void Draw(Graphics g)
+        {
+            using (Brush brush = new SolidBrush(Color))
+            {
+                g.FillRectangle(brush, X, Y, Width, Depth);
+            }
+
+
+        }
+    }
+}
